3ee0f1e664813c53549f9e05dba5cd5c15caed46
Felipe Schmitz <contato.felipeschmitz@gmail.com>
2024-05-16 14:44:47 -0300

    metodo principal criado e metodo run limpado

---
+++ src/main/java/com/devmitz/screenmatch/principal/Principal.java
@@ -0,0 +1,36 @@
+package com.devmitz.screenmatch.principal;
+
+import com.devmitz.screenmatch.model.DadosSerie;
+import com.devmitz.screenmatch.model.DadosTemporada;
+import com.devmitz.screenmatch.service.ConsumoApi;
+import com.devmitz.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner sc = new Scanner(System.in);
+    private final String ENDERECO = "http://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=66f9433c&";
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+    public void ExibeMenu() {
+        System.out.println("Digite o nome da serie para busca: ");
+        var nomeSerie = sc.nextLine();
+
+        var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+
+        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+
+        List<DadosTemporada> dadosTemporadaList = new ArrayList<>();
+		for (int i = 1; i <= dadosSerie.totalTemporadas(); i++ ){
+			json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season="+ i + API_KEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			dadosTemporadaList.add(dadosTemporada);
+		}
+		dadosTemporadaList.forEach(System.out::println);
+    }
+}
